42f0fe1a7921197b50a572e6cd5a9b76b84acdf8
pskalskii <pawelskalski97@gmail.com>
2022-05-06 14:07:00 +0200

    Tablica 5 liczb

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let fiveNumbers = [4, 8, 12, 44, 66];
+
+function giveBack(array) {
+  return array[0];
+}
+
+console.log(giveBack(fiveNumbers));
+console.log(giveBack([20, 45, 77]));
+console.log(giveBack([9, 11, 122]));
